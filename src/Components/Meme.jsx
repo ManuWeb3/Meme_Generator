ac45ded1158ac897bbc431c0b54e6ec256360ac6
ManuWeb3 <manu.web3.0@gmail.com>
2023-03-09 21:35:24 -0700

    <form> -> <div>

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -4,13 +4,13 @@ import '../../styles.css'
 export default function Meme() {
     return (
         <main>
-            <form action='' className="form">
+            <div action='' className="form">
                 <input type="text" name="first-input" className="form-text one" placeholder='Shut up'/>
                 <input type="text" name="second-input" className="form-text two" placeholder='and take my money'/> <br/>
                 <button className="meme-button">
                     <span className="button-text">Get a new meme image 🖼</span>
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
