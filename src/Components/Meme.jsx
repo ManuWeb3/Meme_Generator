1a450d50c8ae683a9934849981612181dd5416a3
ManuWeb3 <manu.web3.0@gmail.com>
2023-03-10 18:42:43 -0700

    Added 100 objects & redering random meme images

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,15 +1,27 @@
 import React from "react"
 import '../../styles.css'
+import memesData from '../../memesData'
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+    
+    function getMemeImage () {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const imageUrl = memesArray[randomNumber].url
+        setMemeImage(() => imageUrl)
+    }
+
     return (
         <main>
             <div action='' className="form">
                 <input type="text" name="first-input" className="form-text one" placeholder='Shut up'/>
                 <input type="text" name="second-input" className="form-text two" placeholder='and take my money'/> <br/>
                 <button className="meme-button">
-                    <span className="button-text">Get a new meme image 🖼</span>
-                </button>
+                    <span className="button-text" onClick={getMemeImage}>Get a new meme image 🖼</span>
+                </button> 
+                <br></br>
+                <img src={memeImage} className='meme-image'/>
             </div>
         </main>
     )
